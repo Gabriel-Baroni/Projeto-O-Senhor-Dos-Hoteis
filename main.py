953acdfc7e21a98116d20e33989afd1cb9db1595
Gabriel-Baroni <gdepaulabaroni@gmail.com>
2024-10-24 23:05:58 -0300

    Testando método GET e POST e tentando conectar ao supabase

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask 
+import os
+from supabase import create_client, Client
+from routes import init_routes
+
 
 app= Flask(__name__)
 
+
+#Configuração do Supabase
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+init_routes(app)
+
 if __name__ == "__main__":
-    app.run
+    app.run()
--- routes.py
+++ routes.py
@@ -1,8 +1,37 @@
-from main import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import supabase 
 
-#Vou colocar aqui as rotas do site
+# Vou colocar aqui as rotas do site
+def init_routes(app):
+    @app.route("/")
+    def homepage():
+        return render_template("index.html")
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")
+    @app.route("/login_cadastro", methods=["GET", "POST"])
+    def login_cadastro():
+        if request.method == "POST":
+            #Pegar os dados do formulário
+            nome = request.form.get("nome")
+            email = request.form.get("email")
+            senha = request.form.get("senha")
+            tel = request.form.get("tel")
+
+            # Cria um dicionário com os dados do usuário
+            data = {
+                "Nome": nome,
+                "email": email,
+                "senha": senha,
+                "tel": tel
+            }
+
+            # Inserir dados no banco de dados do Supabase
+            response = supabase.table("User").insert(data).execute()
+
+            # Verifica o código de status da resposta
+            if response.status_code == 201:  # Sucesso
+                return redirect(url_for('homepage'))  # Corrigido: usa o nome da função
+            else:
+                return "Erro ao cadastrar usuário", 400
+        else:
+            # Renderiza o formulário se o método for GET
+            return render_template("login_cadastro.html")
